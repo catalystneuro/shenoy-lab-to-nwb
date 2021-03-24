c3229ffde2ddb43213de4069f6048930d2d1fd27
Saksham Sharda <saksham20.sharda@gmail.com>
2021-03-24 17:22:02 +0530

    fix cerebus reset

--- churchland_nature_monkey/matextractor.py
+++ churchland_nature_monkey/matextractor.py
@@ -2,8 +2,6 @@ import numpy as np
 import scipy.io as scio
 
 
-
-
 class MatDataExtractor:
 
     def __init__(self, path_r_file, path_n_file):
@@ -32,9 +30,18 @@ class MatDataExtractor:
         """
         if trial_nos is None:
             trial_nos = np.arange(self._no_trials)
-        return np.array([[self.R['CerebusInfoA'][i]['startTime'][0, 0][0, 0],
-                          self.R['CerebusInfoA'][i]['endTime'][0, 0][0, 0]]
-                         for i in trial_nos])
+        trial_times = np.array([[self.R['CerebusInfoA'][i]['startTime'][0, 0][0, 0],
+                                 self.R['CerebusInfoA'][i]['endTime'][0, 0][0, 0]]
+                                for i in trial_nos])
+        split_id = np.where(np.diff(trial_times[:, 0]) < 0)[0][0] + 1
+        # find mean inter_trial times:
+        inter_trial_intervals = np.array(
+            [trial_times[i + 1, 0] - trial_times[i, 1] for i in range(self._no_trials - 1)])
+        inter_trial_intervals = np.delete(inter_trial_intervals, split_id-1)
+        mean_interval = np.mean(inter_trial_intervals)
+        offset_value = trial_times[split_id-1,1]+mean_interval-trial_times[split_id,0]
+        trial_times[split_id:,:]=trial_times[split_id:,:]+offset_value
+        return trial_times, split_id
 
     def _extract_trial_events(self, trial_nos=None):
         """
@@ -43,9 +50,46 @@ class MatDataExtractor:
         if trial_nos is None:
             trial_nos = np.arange(self._no_trials)
         trial_events_dict = dict()
-        events = {'actualFlyAppears':'target_presentation',
+        events = {'actualFlyAppears': 'target_presentation',
                   'actualLandingTime': 'go_cue',
-                  'offlineRT': 'RT'}
+                  'onlineRT': 'RT',
+                  'moveBeginsTime': 'moveBeginsTime',
+                  'moveEndsTime': 'moveEndsTime'}
         for event in events:
-            trial_events_dict.update({events[event]: np.array([self.R[event][i][0,0] for i in trial_nos])/1e3})
+            trial_events_dict.update({events[event]: np.array([self.R[event][i][0, 0]
+                                                               if self.R[event][i].shape[0]!=0
+                                                               else np.nan
+                                                               for i in trial_nos])/1e3})
         return trial_events_dict
+
+    def _extract_trial_details(self, trial_nos=None):
+        """
+                Time in seconds wrt trial start time
+                """
+        if trial_nos is None:
+            trial_nos = np.arange(self._no_trials)
+        trial_events_dict = dict()
+        events = {'possibleRTproblem': 'discardTrial',
+                  'success': 'task_success',
+                  'unhittable': 'unhittable',
+                  'trialType': 'trialType',
+                  'trialVersion': 'trialVersion',
+                  'novelMaze': 'novelMaze'}
+
+        maze_details = {
+            'mazeID': 'maze_ID',
+            'numFlies': 'maze_num_targets',
+            'numBarriers': 'maze_num_barriers',
+            'BARRIER': 'maze_barrier_location',
+            'activeFly': 'accessible_target',
+            'whichFly': 'hit_target',
+            'flySize': 'half_width_target'
+        }
+        for event in events:
+            trial_events_dict.update({events[event]: np.array([self.R[event][i][0, 0] for i in trial_nos])})
+        # discard trials:
+
+        return trial_events_dict
+
+    def _create_unit_details(self, unit_nos=None):
+        pass
--- churchland_nature_monkey/script.py
+++ churchland_nature_monkey/script.py
@@ -6,11 +6,11 @@ rfileloc = r'C:\Users\Saksham\Documents\NWB\shenoy\data\JMaze-LFADS\RC,2009-09-1
 nfileloc = r'C:\Users\Saksham\Documents\NWB\shenoy\data\JMaze-LFADS\N,2009-09-18,1-2,good-ss.mat'
 
 extractor = MatDataExtractor(rfileloc, nfileloc)
+N_fields = extractor.N.dtype.names
+R_fields = extractor.R.dtype.names
 
 trial_ids = extractor._get_trial_ids()
 unit_spike_times = extractor._extract_unit_spike_times()
-trial_times = extractor._extract_trial_times()
+trial_times, split_id = extractor._extract_trial_times()
 inter_trial_intervals = np.array([trial_times[i+1,0]-trial_times[i,1] for i in range(extractor._no_trials-1)])
-trial_change_id = np.where(inter_trial_intervals<0)[0][0]
-inter_trial_intervals = np.delete(inter_trial_intervals, trial_change_id)
 trial_events = extractor._extract_trial_events()
