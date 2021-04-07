47160f7f99d29fb2ccef3b976305c53dfb5ee357
Saksham Sharda <saksham20.sharda@gmail.com>
2021-04-07 15:49:24 +0530

    extract unit spike times from trial sorted

--- churchland_nature_monkey/script.py
+++ churchland_nature_monkey/script.py
@@ -10,7 +10,15 @@ N_fields = extractor.N.dtype.names
 R_fields = extractor.R.dtype.names
 
 trial_ids = extractor._get_trial_ids()
-unit_spike_times = extractor._extract_unit_spike_times()
+trial_spike_times = extractor._extract_unit_spike_times()
 trial_times, split_id = extractor._extract_trial_times()
 inter_trial_intervals = np.array([trial_times[i+1,0]-trial_times[i,1] for i in range(extractor._no_trials-1)])
 trial_events = extractor._extract_trial_events()
+
+# extract channel spike times independent of trial:
+unit_spike_times = []
+for chan in range(len(trial_spike_times[0])):
+    channel_ts = []
+    for trialno in range(extractor._no_trials):
+        channel_ts.extend(trial_spike_times[trialno][chan])
+    unit_spike_times.append(np.array(channel_ts))
