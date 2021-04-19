7f75f11b20a88b215d684d3d0b6f83c74f478daf
Saksham Sharda <saksham20.sharda@gmail.com>
2021-04-19 21:23:02 +0530

    retrieve trials info

--- churchland_nature_monkey/matextractor.py
+++ churchland_nature_monkey/matextractor.py
@@ -47,17 +47,19 @@ class MatDataExtractor:
         """
         if trial_nos is None:
             trial_nos = np.arange(self._no_trials)
-        trial_events_dict = dict()
-        events = {'actualFlyAppears': 'target_presentation',
-                  'actualLandingTime': 'go_cue',
-                  'onlineRT': 'RT',
-                  'moveBeginsTime': 'moveBeginsTime',
-                  'moveEndsTime': 'moveEndsTime'}
+        trial_events_dict = []
+        events = [['actualFlyAppears','target_presentation','time of target presentation'],
+                  ['actualLandingTime','go_cue','time of go cue'],
+                  ['onlineRT','RT','reaction time'],
+                  ['moveBeginsTime','moveBeginsTime','movement onset time'],
+                  ['moveEndsTime','moveEndsTime','movement stop time']]
         for event in events:
-            trial_events_dict.update({events[event]: np.array([self.R[event][i][0, 0]
-                                                               if self.R[event][i].shape[0]!=0
-                                                               else np.nan
-                                                               for i in trial_nos])/1e3})
+            trial_events_dict.append(dict(name=event[1],
+                                          data=np.array([self.R[event[0]][i][0, 0]
+                                                       if self.R[event[0]][i].shape[0]!=0
+                                                       else np.nan
+                                                       for i in trial_nos])/1e3,
+                                          description=event[2]))
         return trial_events_dict
 
     def _extract_trial_details(self, trial_nos=None):
@@ -66,31 +68,41 @@ class MatDataExtractor:
                 """
         if trial_nos is None:
             trial_nos = np.arange(self._no_trials)
-        trial_events_dict = dict()
-        events = {'possibleRTproblem': 'discardTrial',
-                  'success': 'task_success',
-                  'unhittable': 'unhittable',
-                  'trialType': 'trialType',
-                  'trialVersion': 'trialVersion',
-                  'novelMaze': 'novelMaze'}
-
-        maze_details = {
-            'mazeID': 'maze_ID',
-            'numFlies': 'maze_num_targets',
-            'numBarriers': 'maze_num_barriers',
-            'BARRIER': 'maze_barrier_location',
-            'activeFly': 'accessible_target',
-            'whichFly': 'hit_target',
-            'flySize': 'half_width_target'
-        }
+        trial_events_dict = []
+        events = [['possibleRTproblem','discardTrial',"flag that will usually be 0, but is set to 1 if there was a photo box problem (meaning RT can't be calculated accurately) or we had a hand tracking error during the movement. In general, throw those trials away."],
+                  ['success','task_success',"indicates whether the monkey was successful on this trial"],
+                  ['trialType','trialType','trial type'],
+                  ['trialVersion','trialVersion','trial version'],
+                  ['novelMaze','novelMaze','novel maze']]
         for event in events:
-            trial_events_dict.update({events[event]: np.array([self.R[event][i][0, 0] for i in trial_nos])})
-        # discard trials:
-
+            trial_events_dict.append(dict(name=event[1],
+                                          data=np.array([self.R[event[0]][i][0, 0] for i in trial_nos]),
+                                          description=event[2]))
+        maze_details = [
+            ['numFlies','maze_num_targets','number of targets presented'],
+            ['numBarriers','maze_num_barriers','number of barriers presented'],
+            ['activeFly','accessible_target',"indicates which target is accessible (indices match flyX and flyY)."],
+            ['whichFly','hit_target',"indicates which target was hit (indices match flyX and flyY)"]
+        ]
+        for maze_data in maze_details:
+            trial_events_dict.append(dict(name=maze_data[1],
+                                          data=np.array([self.R[maze_data[0]][i][0, 0] for i in trial_nos]),
+                                          description=maze_data[2]))
+        #add target positions/size:
+        target_positions = []
+        for i in trial_nos:
+            target_positions.append(np.concatenate([self.R['PARAMS'][i][0, 0]['flyX'],
+                                                    self.R['PARAMS'][i][0, 0]['flyY']],
+                                                   axis=0))
+        target_size = np.array([self.R['PARAMS'][i][0, 0]['flySize'][0,0] for i in trial_nos])
+        trial_events_dict.append(dict(name='target_position',
+                                      data=target_positions,
+                                      description='x,y position of all targets on screen'))
+        trial_events_dict.append(dict(name='target_size',
+                                      data=target_size,
+                                      description='half width of the targets'))
+        #add barrier position/size:
         return trial_events_dict
-
-    def _create_unit_details(self, unit_nos=None):
-        pass
     
     def _create_behavioral_position(self, trial_nos=None):
         trial_times,_ = self._extract_trial_times()
@@ -101,9 +113,8 @@ class MatDataExtractor:
         offset_hand_Y_jenkins = 8 # offset value, value saved is higher by this amount
         offset_hand_Y_nitschke = 24
         for trial_no in trial_nos:
-            timestamps = np.linspace(trial_times[trial_no,0],
-                                      trial_times[trial_no,1],
-                                      len(self.R['EYE'][trial_no][0,0]['X'].squeeze()))
+            timestamps = trial_times[trial_no,0] + \
+                         np.arange(len(self.R['EYE'][trial_no][0,0]['X'].squeeze()))/1000.0
             eye_positions.append(
                 np.array([self.R['EYE'][trial_no][0,0]['X'].squeeze(),
                          self.R['EYE'][trial_no][0,0]['Y'].squeeze(),
