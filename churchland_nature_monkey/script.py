b217679a3ab1a6ebe2d35b25f7dcc67f696af2d9
Saksham Sharda <saksham20.sharda@gmail.com>
2021-04-20 17:22:19 +0530

    remove lfp read

--- churchland_nature_monkey/script.py
+++ churchland_nature_monkey/script.py
@@ -1,12 +1,11 @@
 from churchland_nature_monkey.matextractor import MatDataExtractor
 import numpy as np
 from pathlib import Path
-from tqdm import tqdm
-import neo
 
-rfileloc = r'C:\Users\Saksham\Documents\NWB\shenoy\data\Jenkins\SpikeSorted\0912\RC,2009-09-12,1-2.mat'
 
-extractor = MatDataExtractor(rfileloc)
+rfileloc = Path(r'C:\Users\Saksham\Documents\NWB\shenoy\data\Jenkins\SpikeSorted\0912\RC,2009-09-12,1-2.mat')
+raw_file_loc = Path(r'C:\Users\Saksham\Documents\NWB\shenoy\data\Jenkins\2009-09-12')
+extractor = MatDataExtractor(rfileloc, raw_file_loc)
 R_fields = extractor.R.dtype.names
 
 trial_ids = extractor._get_trial_ids()
@@ -27,23 +26,4 @@ for chan in range(len(trial_spike_times[0])):
 unit_lookup = extractor.SU['unitLookup'][0,0][:,0]
 array_lookup = extractor.SU['arrayLookup'][0,0][:,0]
 eye_positions, hand_positions, cursor_positions = extractor._create_behavioral_position()
-
-## write to NWB:
-raw_files_names = [['A001','B001'],['A002','B002']]
-raw_file_loc = Path(r'C:\Users\Saksham\Documents\NWB\shenoy\data\Jenkins\2009-09-12')
-lfps=[None]*len(raw_files_names)
-for no, data_part in enumerate(tqdm(raw_files_names)):
-    electrodes = [None]*2
-    for elec_no, electrode in enumerate((data_part)):
-        nev_file = raw_file_loc/f'datafile{electrode}.nev'
-        bk = neo.BlackrockIO(str(nev_file))
-        electrodes[elec_no] = bk.get_analogsignal_chunk()
-        # if ts_all[no] is None:
-        #     ts = bk.get_signal_t_start(block_index=0, seg_index=0) + np.arange(electrodes[elec_no].shape[0]/1000.0)
-        #     if no>1:
-        #         ts=ts+ts_all[0][-1] + 1/1000.0
-        #     ts_all[no] = ts
-    elec_common_len = np.min([electrodes[i].shape[0] for i in range(2)])
-    electrodes = [electrodes[i][:elec_common_len, :] for i in range(2)]
-    lfps[no] = np.concatenate(electrodes, axis=1)
-lfp_data = np.concatenate(lfps, axis=0) if len(lfps) > 1 else lfps[0]
+lfp_data = extractor.extract_lfp()
