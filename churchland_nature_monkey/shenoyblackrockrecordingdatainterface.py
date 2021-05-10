d5d683de8c5373c8b7b0acaef63fcee079f6ccd8
Saksham Sharda <saksham20.sharda@gmail.com>
2021-05-10 18:14:39 +0530

    electricalseries names differ for each nsx loc

--- churchland_nature_monkey/shenoyblackrockrecordingdatainterface.py
+++ churchland_nature_monkey/shenoyblackrockrecordingdatainterface.py
@@ -1,13 +1,7 @@
-import numpy as np
-
 from nwb_conversion_tools import BlackrockRecordingExtractorInterface
-from spikeextractors import BlackrockRecordingExtractor, \
-    MultiRecordingChannelExtractor, MultiRecordingTimeExtractor, NwbRecordingExtractor
+
 from pathlib import Path
-from pynwb.file import NWBFile
 from typing import Union
-from nwb_conversion_tools import NWBConverter
-from nwb_conversion_tools.json_schema_utils import get_base_schema
 
 PathType = Union[str, Path, None]
 
@@ -16,19 +10,27 @@ class ShenoyBlackRockRecordingDataInterface(BlackrockRecordingExtractorInterface
 
     def __init__(self, filename):
         self.nsx_loc = Path(filename)
-        super().__init__(filename=self.nsx_loc)
-        if self.nsx_loc.name[0] == 'B':
+        super().__init__(filename=filename)
+        if 'B' in self.nsx_loc.name:
             self._group_name = ['M1 array']
-            self.recording_extractor._channel_ids += 96
+            self.recording_extractor._channel_ids = [i + 96 for i in self.recording_extractor._channel_ids]
         else:
             self._group_name = ['PMd array']
+        self.recording_extractor.clear_channels_property('name')
 
     def get_metadata(self):
-        metadata = super().get_metadata()
-        metadata['Ecephys']['ElectricalSeries'].update(
-            name=self.nsx_loc.name,
-            description=f'LFP signal for array{self.nsx_loc.name[0]} '
-                        f'segment{self.nsx_loc.name[-1]}')
+        metadata = dict()
+        metadata['Ecephys'] = dict(
+            Device=[dict(
+                name='Device_ecephys',
+                description='no description'
+            )],
+            ElectrodeGroup=[],
+        )
+        metadata['Ecephys'].update(
+            {f'ElectricalSeries{self.nsx_loc.stem[-4:]}': dict(
+                name=self.nsx_loc.stem[-4:],
+                description=f'LFP signal for array{self.nsx_loc.stem[8]} segment {self.nsx_loc.stem[-1]}')})
         metadata['Ecephys']['Device'] = [dict(name='Utah Array(PMd)',
                                               description='96 channel utah array',
                                               manufacturer='BlackRock Microsystems'),
@@ -43,10 +45,12 @@ class ShenoyBlackRockRecordingDataInterface(BlackrockRecordingExtractorInterface
                                                       description='',
                                                       location='M1 in Motor Cortex, left hemisphere',
                                                       device_name='Utah Array(M1)')]
-        metadata['Electrodes'] = [dict(name='filtering',
-                                       data=['1000Hz']*96),
-                                  dict(name='group_name',
-                                       data=self._group_name * 96)
-                                  ]
+        metadata['Ecephys']['Electrodes'] = [dict(name='filtering',
+                                                  description='filtering',
+                                                  data=['1000Hz']*96),
+                                             dict(name='group_name',
+                                                  description='electrode group name',
+                                                  data=self._group_name*96)
+                                             ]
 
         return metadata
