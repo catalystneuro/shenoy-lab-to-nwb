e478eaa7b26300ead7f7ba3a2ebcfd44b28eb53b
Saksham Sharda <saksham20.sharda@gmail.com>
2021-07-27 11:38:53 +0530

    running parallel

--- center_out_task/conversion_script.py
+++ center_out_task/conversion_script.py
@@ -1,6 +1,6 @@
 from .coutnwbconverter import COutNWBConverter
 from pathlib import Path
-
+from joblib import Parallel, delayed
 
 def convert(source_folder):
 # retrieve the correct files from source path:
@@ -18,13 +18,14 @@ def convert(source_folder):
         arg_name = array+nsx_file.stem[-1]
         source_data.update({arg_name:dict(filename='',nsx_override=str(nsx_file))})
         conversion_options.update({arg_name:dict(
-            es_key=f"ElectricalSeries_{nsx_file.parent.stem+'_'+nsx_file.stem[-1]}")})
+            es_key=f"ElectricalSeries_{nsx_file.parent.stem+'_'+nsx_file.stem[-1]}",
+            write_as='lfp')})
     source_data.update(Mat=dict(filename=str(mat_file)),
                        Movie=dict(movie_filepath=str(movie_file)))
-    conversion_options.update(Movie=dict(external_mode=True))
+    conversion_options.update(Movie=dict(external_mode=False))
 
     ch = COutNWBConverter(source_data)
-    nwbfile_saveloc = source_folder / f"{source_folder.name}_nwb_v3.nwb"
+    nwbfile_saveloc = source_folder / f"{source_folder.name}_nwb_v4.nwb"
 
     print("running conversion to nwb...")
     ch.run_conversion(
@@ -41,3 +42,7 @@ if __name__ == '__main__':
         r"C:\Users\Saksham\Documents\NWB\shenoy\data\centerOut\3Ring\2016-01-28 (1)"
     )
     convert(source_folder)
+
+def run_parrallel(pt):
+    pt = Path(pt)
+    Parallel(n_jobs=10)(delayed(convert)(loc) for loc in pt.glob('*/*'))
