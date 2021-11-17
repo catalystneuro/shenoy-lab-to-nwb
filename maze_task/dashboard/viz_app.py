1bae9ac424a28f2dff7603828af2a6687e1aa1ba
Saksham Sharda <saksham20.sharda@gmail.com>
2021-11-17 09:18:02 +0530

    pep

--- maze_task/dashboard/viz_app.py
+++ maze_task/dashboard/viz_app.py
@@ -1,19 +1,17 @@
-import dash_core_components as dcc
-import dash_html_components as html
-import dash
-from dash.dependencies import Input, Output, State
-from pynwb import NWBHDF5IO
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import numpy as np
-from nwbwidgets.utils.timeseries import timeseries_time_to_ind
-import dash_labs as dl
 from uuid import uuid4
-import diskcache
-from flask_caching import Cache
 
+import dash
+import dash_labs as dl
 ## Diskcache
 import diskcache
+import numpy as np
+import plotly.graph_objects as go
+from dash import dcc, html
+from flask_caching import Cache
+from nwbwidgets.utils.timeseries import timeseries_time_to_ind
+from plotly.subplots import make_subplots
+from pynwb import NWBHDF5IO
+
 launch_uid = uuid4()
 cache = diskcache.Cache("./cache")
 long_callback_manager = dl.plugins.DiskcacheCachingCallbackManager(
@@ -38,7 +36,7 @@ cache = Cache()
 cache.init_app(app.server, config=CACHE_CONFIG)
 
 
-def trialize_time_series(trials,time_series):
+def trialize_time_series(trials, time_series):
     trl_ts = []
     for trial_no in range(len(trials)):
         start_id = timeseries_time_to_ind(time_series, trials['start_time'][trial_no])
@@ -58,31 +56,31 @@ app.layout = html.Div([
                         disabled=False)]),
     html.Br(),
     html.Div(['trial version selector',
-        dcc.Dropdown(
-            id="trial-version-dropdown",
-            options=[],
-            disabled=True,
-            multi=True
-    ),'trial type selector',
-        dcc.Dropdown(
-            id="trial-type-dropdown",
-            options=[],
-            disabled=True,
-            multi=True
-    ),'hit submit after selection',
-        html.Button(
-            id='submit-button-state',
-            n_clicks=0,
-            children='Submit',
-            disabled=True),
-        html.Button(
-            id='cancel-button-state',
-            n_clicks=0,
-            children='Cancel Plot',
-            disabled=True),
-        html.Progress(id="progress_bar_extraction"),
-        # html.Progress(id="progress_bar_plotting")
-    ]),
+              dcc.Dropdown(
+                  id="trial-version-dropdown",
+                  options=[],
+                  disabled=True,
+                  multi=True
+              ), 'trial type selector',
+              dcc.Dropdown(
+                  id="trial-type-dropdown",
+                  options=[],
+                  disabled=True,
+                  multi=True
+              ), 'hit submit after selection',
+              html.Button(
+                  id='submit-button-state',
+                  n_clicks=0,
+                  children='Submit',
+                  disabled=True),
+              html.Button(
+                  id='cancel-button-state',
+                  n_clicks=0,
+                  children='Cancel Plot',
+                  disabled=True),
+              html.Progress(id="progress_bar_extraction"),
+              # html.Progress(id="progress_bar_plotting")
+              ]),
     dcc.Graph(id="trial-subplots"),
 ])
 
@@ -103,13 +101,13 @@ def global_store(fileloc):
 
 @app.callback(
     output=(
-        dl.Output('trial-version-dropdown','options'),
-        dl.Output('trial-type-dropdown','options'),
-        dl.Output('trial-version-dropdown','disabled'),
-        dl.Output('trial-type-dropdown','disabled'),
+            dl.Output('trial-version-dropdown', 'options'),
+            dl.Output('trial-type-dropdown', 'options'),
+            dl.Output('trial-version-dropdown', 'disabled'),
+            dl.Output('trial-type-dropdown', 'disabled'),
     ),
     args=(
-        dl.Input('nwb-input','value'),
+            dl.Input('nwb-input', 'value'),
     )
 )
 def open_nwb(fileloc):
@@ -124,24 +122,24 @@ def open_nwb(fileloc):
             return (version_ops, type_ops, False, False)
         except Exception as e:
             print(e)
-    return ([],[], True,True)
+    return ([], [], True, True)
 
 
 @app.long_callback(
-    output=dl.Output('trial-subplots','figure'),
-    args=(dl.Input('submit-button-state','n_clicks'),
-          dl.State('nwb-input','value'),
-          dl.State('trial-version-dropdown','value'),
-          dl.State('trial-type-dropdown','value')),
+    output=dl.Output('trial-subplots', 'figure'),
+    args=(dl.Input('submit-button-state', 'n_clicks'),
+          dl.State('nwb-input', 'value'),
+          dl.State('trial-version-dropdown', 'value'),
+          dl.State('trial-type-dropdown', 'value')),
     running=[
         (dl.Output("submit-button-state", "disabled"), True, False),
         (dl.Output("cancel-button-state", "disabled"), False, True),
     ],
     cancel=[dl.Input("cancel-button-state", "n_clicks")],
     progress=dl.Output("progress_bar_extraction", ("value", "max"))
-              # dl.Output("progress_bar_plotting", ("value", "max")))
+    # dl.Output("progress_bar_plotting", ("value", "max")))
 )
-def draw_graphs(set_progress,args):
+def draw_graphs(set_progress, args):
     n_clicks, fileloc, versions, types = args
     print('long callback run')
     if versions is not None and types is not None:
@@ -151,21 +149,20 @@ def draw_graphs(set_progress,args):
         cursor = nwbfile.processing['behavior'].data_interfaces['Position'].spatial_series['Cursor']
         trials_version_data = trials['trial_version'].data[()]
         trials_type_data = trials['trial_type'].data[()]
-        trialized_ts = trialize_time_series(trials,cursor)
+        trialized_ts = trialize_time_series(trials, cursor)
         titles = [f'trial version {version}, trial type {type}'
                   for type in types for version in versions]
         fig = make_subplots(len(versions), len(types),
-                            shared_xaxes=True,shared_yaxes=True,
+                            shared_xaxes=True, shared_yaxes=True,
                             vertical_spacing=0.2,
                             subplot_titles=titles,
                             horizontal_spacing=0.2)
 
-
-        #plot the barriers and targets for the given trial version and trial type:
+        # plot the barriers and targets for the given trial version and trial type:
         tot_rows = len(versions)
         tot_cols = len(types)
         trial_rows = np.empty((tot_rows, tot_cols), dtype=object)
-        trial_rows.fill(np.array([],dtype=int))
+        trial_rows.fill(np.array([], dtype=int))
         cursor_trajectory = np.empty((tot_rows, tot_cols), dtype=object)
         cursor_trajectory.fill(np.nan*np.ones((1, cursor.data.shape[1])))
         for trial_no in range(len(trials)):
@@ -176,12 +173,12 @@ def draw_graphs(set_progress,args):
                 trial_rows[a, b] = np.append(trial_rows[a, b], trial_no)
                 cursor_trajectory[a, b] = np.vstack([cursor_trajectory[a, b], trialized_ts[trial_no]])
         c = 0
-        print(tot_rows,tot_cols)
+        print(tot_rows, tot_cols)
         for a in range(tot_rows):
             for b in range(tot_cols):
                 c += 1
-                set_progress((str(c), str(tot_rows * tot_cols)))
-                if len(trial_rows[a,b]) == 0:
+                set_progress((str(c), str(tot_rows*tot_cols)))
+                if len(trial_rows[a, b]) == 0:
                     continue
                 trial_row = trial_rows[a, b][0]
                 # plot target positions:
@@ -209,7 +206,7 @@ def draw_graphs(set_progress,args):
                                            line_color='red'),
                               row=a + 1, col=b + 1)
 
-        fig.update_layout(height=1000,width=1000)
+        fig.update_layout(height=1000, width=1000)
         return fig
     else:
         return go.FigureWidget()
