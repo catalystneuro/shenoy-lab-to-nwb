5db95f4a4df48cfea3f1cd0435de72cda3f65c49
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-07-22 07:44:34 -0700

    reorganized files/imports

--- maze_task_unsorted/maze_task_unsorted_convert_session.py
+++ maze_task_unsorted/maze_task_unsorted_convert_session.py
@@ -1,72 +1,10 @@
-from neuroconv.utils import FolderPathType, FilePathType, load_dict_from_file, dict_deep_update
-from neuroconv.datainterfaces import BlackrockRecordingInterface
-from neuroconv import NWBConverter
+from neuroconv.utils import FolderPathType, load_dict_from_file, dict_deep_update
 from pathlib import Path
 import shutil
 from pytz import timezone
 from datetime import datetime
-from spikeinterface.extractors import BlackrockRecordingExtractor
 
-class ShenoyBlackrockRecordingInterface(BlackrockRecordingInterface):
-    Extractor = BlackrockRecordingExtractor
-
-    def __init__(self, file_path: FilePathType, es_key: str = "ElectricalSeries"):
-        super().__init__(file_path=file_path, es_key=es_key)
-        if "B" in file_path.name:
-            self._region = "M1 Motor Cortex"
-            self.recording_extractor._channel_ids = [
-                i + 96 for i in self.recording_extractor._channel_ids
-            ]
-            self.recording_extractor.set_channel_groups([2] * 96)
-        else:
-            self._region = "Pre-Motor Cortex, dorsal"
-            self.recording_extractor.set_channel_groups([1] * 96)
-        self.recording_extractor.set_property("filtering", ["1000Hz"]*96)
-        self.recording_extractor.set_property("brain_area", [self._region]*96)
-
-    def get_metadata(self):
-        metadata = super().get_metadata()
-
-        file_path = self.source_data["file_path"]
-        es_key = self.es_key
-
-        metadata["Ecephys"][es_key]["description"] = (
-        f"LFP signal for array {file_path.stem[8]}, segment {file_path.stem[-1]}"
-        f"data for both arrays A,B in the same segment should be, but is not of the same time length"
-        f"and cannot be synced due to lack of time stamps. Ignore the starting times."
-        )
-        metadata["Ecephys"]["Device"] = [
-            dict(
-                name="Utah Array(PMd)",
-                description="96 channel utah array",
-                manufacturer="BlackRock Microsystems",
-            ),
-            dict(
-                name="Utah Array(M1)",
-                description="96 channel utah array",
-                manufacturer="BlackRock Microsystems",
-            ),
-        ]
-        metadata["Ecephys"]["ElectrodeGroup"] = [
-            dict(
-                name="1",
-                description="array corresponding to device implanted at PMd",
-                location="Caudal, dorsal Pre-motor cortex, Left hemisphere",
-                device="Utah Array(PMd)",
-            ),
-            dict(
-                name="2",
-                description="array corresponding to device implanted at M1",
-                location="M1 in Motor Cortex, left hemisphere",
-                device="Utah Array(M1)",
-            ),
-        ]
-        return metadata
-
-class MazeTaskUnsortedNWBConverter(NWBConverter):
-    data_interface_classes = dict(
-        BlackRock=ShenoyBlackrockRecordingInterface,
-    )
+from maze_task_unsorted.maze_task_unsorted_nwbconverter import MazeTaskUnsortedNWBConverter
 
 def session_to_nwb(
     *,
